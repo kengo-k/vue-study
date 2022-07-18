f1a9d0749c396a2cec22ee621e8847497c303b78
kengo-k <kurobanekengo@gmail.com>
2022-07-18 17:11:57 +0900

    add first sfc example

---
+++ 03-sfc/01-hello/src/modules/TestComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>{{message}}</div>
+</template>
+
+<script>
+export default {
+  name: "TestComponent",
+  data() {
+    return {
+      message: "Hello Vue SFC!"
+    }
+  }
+}
+</script>
+
+// divタグに対しグローバルにcssを適用してしまう
+<style>
+  div {
+    color: blue;
+  }
+</style>
+
+// scopedを指定することでこのコンポーネント内に閉じたスタイルを定義できる
+<style scoped>
+  div {
+    color: pink;
+    font-size: xx-large;
+  }
+</style>
