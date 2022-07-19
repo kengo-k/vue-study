3ff2808a947ed8f6f94c1b7d260d4642917750b0
kengo-k <kurobanekengo@gmail.com>
2022-07-19 18:39:27 +0900

    03-sfc/03: add error check

--- 03-sfc/03-router/src/components/BookDetail.vue
+++ 03-sfc/03-router/src/components/BookDetail.vue
@@ -12,6 +12,21 @@ export default {
     id: Number,
     title: String,
     content: String
+  },
+  data() {
+    return {
+      bookIndex: this.$route.params.id - 1,
+      books: [
+        { id: 1, title: "title1", content: "content1" },
+        { id: 2, title: "title2", content: "content2" },
+        { id: 3, title: "title3", content: "content3" }
+      ]
+    }
+  },
+  created() {
+    if (this.$route.params.id > this.books.length) {
+      this.$router.push("/book");
+    }
   }
 }
 </script>
