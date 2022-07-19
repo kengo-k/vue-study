432c73b667e2f1a0aa5b7f9a0ed7a94dcc6724ee
kengo-k <kurobanekengo@gmail.com>
2022-07-19 18:33:19 +0900

    03-sfc/03: add route with props example

---
+++ 03-sfc/03-router/src/components/BookDetail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>本の詳細</p>
+    <p>タイトル: {{title}}</p>
+    <p>コンテント: {{content}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: Number,
+    title: String,
+    content: String
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 03-sfc/03-router/src/views/BookList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>BookList</h1>
+    <ul>
+      <li @click="showBookDetail(book.id)" v-for="(book,index) in books"  :key="book.id">
+        [{{index}}] {{book.title}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookList",
+  data() {
+    return {
+      books: [
+        { id: 1, title: "title1", content: "content1" },
+        { id: 2, title: "title2", content: "content2" },
+        { id: 3, title: "title3", content: "content3" }
+      ]
+    }
+  },
+  methods: {
+    showBookDetail(id) {
+      this.bookIndex = id - 1;
+      this.$router.push({
+        name: "Book",
+        params: {
+          id: this.books[this.bookIndex].id,
+          title: this.books[this.bookIndex].title,
+          content: this.books[this.bookIndex].content
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
