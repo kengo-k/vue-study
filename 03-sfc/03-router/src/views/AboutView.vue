c5d55961639618fb90a7e0aa30f40b333eb13e04
kengo-k <kurobanekengo@gmail.com>
2022-07-19 18:22:47 +0900

    03-sfc/03: add  usage example

--- 03-sfc/03-router/src/views/AboutView.vue
+++ 03-sfc/03-router/src/views/AboutView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="checkRouteInfo">ルート情報確認</button>
+    <button @click="gotoHome">Homeに戻る</button>
   </div>
 </template>
+
+<script>
+export default {
+  name: "AboutComponent",
+  methods: {
+    checkRouteInfo() {
+      // this.$routeで現在<route-view>に表示しているコンポーネントのルート情報を取得できる
+      console.log(this.$route)
+    },
+    gotoHome() {
+      this.$router.push("/");
+    }
+  }
+}
+</script>
