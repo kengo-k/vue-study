82d5f334c42a9c14546e970d4a9bf52946d89db8
kengo-k <kurobanekengo@gmail.com>
2022-07-20 17:59:50 +0900

    03-sfc/05: basic store usage(dispatch, commit)

--- 03-sfc/05-vuex/src/store/index.js
+++ 03-sfc/05-vuex/src/store/index.js
@@ -5,12 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 0
   },
   getters: {
   },
   mutations: {
+    increment(state) {
+      state.count++;
+    },
+    addCount(state, payload) {
+      state.count += payload.value;
+    }
   },
   actions: {
+    incrementAction({ commit }) {
+      commit("increment");
+    },
+    addCountAction({ commit }, payload) {
+      commit("addCount", payload);
+    }
   },
   modules: {
   }
