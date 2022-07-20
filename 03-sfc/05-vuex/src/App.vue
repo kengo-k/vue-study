e65bec505da87e4649596d85d109a2fbf30f4a8c
kengo-k <kurobanekengo@gmail.com>
2022-07-20 18:13:51 +0900

    03-sfc/05: getters example

--- 03-sfc/05-vuex/src/App.vue
+++ 03-sfc/05-vuex/src/App.vue
@@ -6,5 +6,26 @@
     </nav>
     <router-view/>
     {{$store.state.count}}
+    <br/>
+    <ul>
+      <li v-for="(u, index) in visibleUsers" :key="u.id">
+        [{{index}}]: id={{u.id}}, name={{u.name}}
+      </li>
+    </ul>
+    <br/>
+    {{getUserById}}
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    visibleUsers() {
+      return this.$store.getters.visibleUsers;
+    },
+    getUserById() {
+      return this.$store.getters.getUserById(2);
+    }
+  }
+};
+</script>
--- 03-sfc/05-vuex/src/store/index.js
+++ 03-sfc/05-vuex/src/store/index.js
@@ -5,9 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    count: 0
+    count: 0,
+    users: [
+      {id: 1, name: "yamada", isVisible: true},
+      {id: 2, name: "tanaka", isVisible: false},
+      {id: 3, name: "suzuki", isVisible: true},
+    ]
   },
   getters: {
+    visibleUsers(state) {
+      return state.users.filter(u => u.isVisible);
+    },
+    getUserById: state => id => {
+      return state.users.find(u => u.id +++ id);
+    }
   },
   mutations: {
     increment(state) {
