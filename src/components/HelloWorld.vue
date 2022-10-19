68dd2a0d1a8db0600a30ae1f8c1d4137e6d61fb9
kengo-k <kurobanekengo@gmail.com>
2022-10-19 23:39:58 +0900

    v-bindのサンプルを追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>Hello, {{ name }}!!</div>
+
+  <h1>dataの値を画面に表示する</h1>
+  <article>
+    data()が返したオブジェクトは{{"\{\{ プロパティ名 \}\}"}}で画面に表示することができる。
+    <ul>
+      <li>テンプレート: Hello, {{"\{\{ name \}\}"}}</li>
+      <li>実際の表示: Hello, {{ name }}!!</li>
+    </ul>
+  </article>
+
+  <h1>dataの値を属性に設定する</h1>
+  <article>
+    HTML要素の属性にdataの値を設定する場合は上記のような{{"\{\{ プロパティ名 \}\}"}}の構文を使うことができない。
+    <div>
+      例: {{ wrongAttributeBindExample }}
+    </div>
+    属性にdataの値を設定するにはv-bindを使用する
+    <div>
+      例: {{ vBindExample }} <input type="text" v-bind:value="name" />
+    </div>
+    v-bindを指定する場合は{{ mustache }}ではなくダブルクオートの中に直接プロパティ名を指定する。固定文字列"name"を入れているわけではないので注意。
+  </article>
+
 </template>
 
 <script>
@@ -7,7 +29,10 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      name: "Vue"
+      name: "Vue",
+      mustache: "{{ ... }}",
+      wrongAttributeBindExample: "<input type=\"text\" value=\"{{ name }}\" />",
+      vBindExample: "<input type=\"text\" v-bind:value=\"name\" />"
     }
   }
 };
