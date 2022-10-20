f24a9936f825bf5a81e839e652d6c9ed9edf6d5e
kengo-k <kurobanekengo@gmail.com>
2022-10-20 09:13:05 +0900

    add tailwindcss

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import 'mdb-vue-ui-kit/css/mdb.min.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './index.css'
 
 createApp(App).use(router).mount('#app')
