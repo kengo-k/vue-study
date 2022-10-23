940781a8b8df746bf48bed4aa39fd6c970fd6310
kengo-k <kurobanekengo@gmail.com>
2022-10-23 11:58:03 +0900

    add topic 04: v-for

--- src/router/index.js
+++ src/router/index.js
@@ -2,28 +2,14 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Topic01VBind from '../views/Topic01VBind.vue'
 import Topic02VShow from '../views/Topic02VShow.vue'
 import Topic03VIf from '../views/Topic03VIf.vue'
+import Topic04VFor from '../views/Topic04VFor.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Topic01VBind',
-    component: Topic01VBind
-  },
-  {
-    path: '/01',
-    name: 'Topic01VBind',
-    component: Topic01VBind
-  },
-  {
-    path: '/02',
-    name: 'Topic02VShow',
-    component: Topic02VShow
-  },
-  {
-    path: '/03',
-    name: 'Topic03VIf',
-    component: Topic03VIf
-  }
+  { path: '/', name: 'Topic01VBind', component: Topic01VBind },
+  { path: '/01', name: 'Topic01VBind', component: Topic01VBind },
+  { path: '/02', name: 'Topic02VShow', component: Topic02VShow },
+  { path: '/03', name: 'Topic03VIf', component: Topic03VIf },
+  { path: '/04', name: 'Topic04VFor', component: Topic04VFor },
 ]
 
 const router = createRouter({
---
+++ src/views/Topic04VFor.vue
@@ -0,0 +1,54 @@
+<template>
+
+  <div class="border-2 m-4">
+    <ul>
+      <li v-for="name in names" :key="name">
+        name is {{ name }}
+      </li>
+    </ul>
+
+    v-for="item in items"で配列itemsから要素を先頭から取り出しながらループする。
+    inではなくofでも動作する
+
+    <ul>
+      <li v-for="name of names" :key="name">
+        name is {{ name }}
+      </li>
+    </ul>
+
+    値を取り出すとき指定する要素の変数に続けて2番目の変数を指定するとインデックスを受け取ることができる。
+    <ul>
+      <li v-for="(name, i) of names" :key="name">
+        name[{{ i }}] is {{ name }}
+      </li>
+    </ul>
+
+    配列だけでなくオブジェクトの要素でループすることもできる。引数は最大で三つまで取れる。先頭から順に、値、名前、インデックスとなる。
+    <ul>
+      <li v-for="(value, key, i) in bookInfo" :key="i">
+        {{ i }}: {{ key }} = {{ value }}
+      </li>
+    </ul>
+
+  </div>
+
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: "Topic04VFor",
+  data() {
+    return {
+      names: ["Yamada", "Tanaka", "Suzuki"],
+      bookInfo: {
+        title: "Vue Basics",
+        author: "Yamada Taro",
+        price: 2980
+      }
+    }
+  }
+};
+</script>
