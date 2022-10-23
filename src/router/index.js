6f214628db99a4262f2ecb229275ea0211e1d145
kengo-k <kurobanekengo@gmail.com>
2022-10-23 11:37:46 +0900

    add topic 03: v-if

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Topic01VBind from '../views/Topic01VBind.vue'
 import Topic02VShow from '../views/Topic02VShow.vue'
+import Topic03VIf from '../views/Topic03VIf.vue'
 
 const routes = [
   {
@@ -17,6 +18,11 @@ const routes = [
     path: '/02',
     name: 'Topic02VShow',
     component: Topic02VShow
+  },
+  {
+    path: '/03',
+    name: 'Topic03VIf',
+    component: Topic03VIf
   }
 ]
 
---
+++ src/views/Topic03VIf.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <div class="border-2 m-4">
+    <div v-if="isVisible">何も表示されません</div>
+    v-showによる非表示時にはdisplay:noneにより画面に表示されなくなるがv-ifの場合は要素そのものが出力されなくなる。
+
+    <div v-if="status === 404">Not Found</div>
+    <div v-else-if="status === 500">Server Error</div>
+    <div v-else>Success</div>
+
+    v-if, v-else-if, v-elseは上から順番に並んで記述しなければならない。途中に要素などを追加するとエラーになる。
+  </div>
+
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: "Topic03VIf",
+  data() {
+    return {
+      isVisible: false,
+      status: 200,
+    }
+  }
+};
+</script>
