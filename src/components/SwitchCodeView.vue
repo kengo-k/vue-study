1e1f60e41270cc68f86b7ec54656a273ef0056f9
kengo-k <kurobanekengo@gmail.com>
2022-10-23 19:44:14 +0900

    タブ周りのスタイルの不具合を修正

--- src/components/SwitchCodeView.vue
+++ src/components/SwitchCodeView.vue
@@ -1,19 +1,15 @@
 <template>
   <div class="inline-flex border-b border-gray-200 dark:border-gray-700">
     <button
-      v-on:click="selectedTab = 1"
-      class="h-10 px-4 py-2 -mb-px text-sm text-center bg-transparent border-b-2 sm:text-base whitespace-nowrap focus:outline-none
-      text-blue-600 border-blue-500
-    ">
-        Code
+      v-on:click="switchTab(1)"
+      :class="viewClass">
+      Code
     </button>
 
     <button
-      v-on:click="selectedTab = 2"
-      class="h-10 px-4 py-2 -mb-px text-sm text-center  bg-transparent border-b-2 sm:text-base whitespace-nowrap cursor-base focus:outline-none
-      text-gray-700 hover:border-gray-400 border-transparent
-    ">
-        View
+      v-on:click="switchTab(2)"
+      :class="codeClass">
+      View
     </button>
   </div>
   <div v-show="selectedTab === 1">
@@ -25,6 +21,17 @@
 </template>
 
 <script>
+const buildClass = (classes) => {
+  const clsObj = {}
+  for(const cls of classes) {
+    clsObj[cls] = true
+  }
+  return clsObj
+}
+const baseClasses = ["h-10", "px-4", "py-2", "-mb-px", "text-sm", "text-center", "bg-transparent", "border-b-2", "sm:text-base", "whitespace-nowrap", "focus:outline-none"];
+const baseClass = buildClass(baseClasses)
+const activeClass = Object.assign({}, baseClass, buildClass(["text-blue-600", "border-blue-500"]))
+const inactiveClass = Object.assign({}, baseClass, buildClass(["text-gray-700", "hover:border-gray-400", "border-transparent"]))
 import Code from "@/components/Code.vue";
 export default {
   name: "SwitchCodeView",
@@ -36,7 +43,23 @@ export default {
   },
   data() {
     return {
-      selectedTab: 1
+      selectedTab: 1,
+      viewClass: activeClass,
+      codeClass: inactiveClass
+    }
+  },
+  methods: {
+    switchTab(tabIndex) {
+      if (tabIndex === 1) {
+        this.selectedTab = tabIndex
+        this.viewClass = activeClass
+        this.codeClass = inactiveClass
+      }
+      if (tabIndex === 2) {
+        this.selectedTab = tabIndex
+        this.viewClass = inactiveClass
+        this.codeClass = activeClass
+      }
     }
   }
 };
